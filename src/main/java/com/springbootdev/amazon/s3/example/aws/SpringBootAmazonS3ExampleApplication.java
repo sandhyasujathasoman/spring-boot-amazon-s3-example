8a94d3000983d139c1b0c69ef7bfb096421bf73a
S S <368221@cognizant.com>
2019-05-30 11:58:13 +0530

    wrapper .mvn added

--- src/main/java/com/springbootdev/amazon/s3/example/aws/SpringBootAmazonS3ExampleApplication.java
+++ src/main/java/com/springbootdev/amazon/s3/example/aws/SpringBootAmazonS3ExampleApplication.java
@@ -1,10 +1,16 @@
 package com.springbootdev.amazon.s3.example.aws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootAmazonS3ExampleApplication {
     public static void main(String[] args) {
